a4bb14346488e0f098f31a107d9aff99d2f3d2bb
Liming <liming_yin18@outlook.com>
2021-10-06 20:14:22 +0800

    init

---
+++ Client/build/CMakeFiles/Cl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cl.dir/Client.cpp.o"
+  "Cl"
+  "Cl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
